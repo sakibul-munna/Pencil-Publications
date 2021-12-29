850e3b08c60d8a056b1e6f26995b703bbb09a0d6
Sakibul_Munna <sakibul.munnaa@gmail.com>
2021-12-29 16:13:05 +0600

    The basic structure created and some packages have been installed.

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Pencil Publications !!!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on Port ${port}...`);
+});
