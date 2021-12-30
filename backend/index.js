1037d81617648a1e95b6149297f3e96ad79a66a9
Sakibul_Munna <sakibul.munnaa@gmail.com>
2021-12-30 23:18:14 +0600

    Connected to local mongodb database.

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+require("./startup/db")();
+
 app.get("/", (req, res) => {
   res.send("Welcome to Pencil Publications !!!");
 });
---
+++ backend/startup/db.js
@@ -0,0 +1,18 @@
+const winston = require("winston");
+const mongoose = require("mongoose");
+const config = require("config");
+
+module.exports = function () {
+  const db = "mongodb://localhost/pencil";
+  mongoose
+    .connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log(`Connected to ${db} database...`);
+    })
+    .catch((ex) => {
+      console.log("Could Not Connect to MongoDB..." + ex);
+    });
+};
