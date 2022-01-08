39b89cff225a8f45eda7ecf24765c4d8669daff9
Sakibul_Munna <sakibul.munnaa@gmail.com>
2022-01-08 15:44:50 +0600

    Error Handling and Logging.
    Used winston to log the errors. The logging.js file of startup folder will do the job. And the errorHandler middleware will be looking for any kind of error.

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const app = express();
+const winston = require("winston");
 
 require("./startup/config")();
 require("./startup/routes")(app);
 require("./startup/db")();
+require("./startup/logging")();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to Pencil Publications !!!");
+  res.send("Welcome to Pencil Publications Backend !!!");
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Listening on Port ${port}...`);
+  winston.info(`Listening on Port ${port}...`);
 });
--- backend/startup/routes.js
+++ backend/startup/routes.js
@@ -4,6 +4,7 @@ const books = require("../routes/books");
 const authors = require("../routes/authors");
 const admins = require("../routes/admins");
 const auth = require("../routes/auth");
+const errorHandler = require("../middleware/errorHandler");
 
 module.exports = function (app) {
   app.use(express.json());
@@ -12,4 +13,5 @@ module.exports = function (app) {
   app.use("/api/authors", authors);
   app.use("/api/admins", admins);
   app.use("/api/auth", auth);
+  app.use(errorHandler);
 };
