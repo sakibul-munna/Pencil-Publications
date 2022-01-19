68d9af9e3ddce5844ab7d30488f65b7b4e40b1cd
Sakibul_Munna <sakibul.munnaa@gmail.com>
2022-01-19 17:36:35 +0600

    Integration Test of Get By Id method of Genres Module.

--- backend/tests/integration/genres.test.js
+++ backend/tests/integration/genres.test.js
@@ -1,4 +1,5 @@
 const request = require("supertest");
+var mongoose = require("mongoose");
 let server;
 
 const { Genre } = require("../../models/genre");
@@ -23,6 +24,31 @@ describe("/api/genres", () => {
       expect(res.body.length).toBe(2);
       expect(res.body.some((x) => x.name === "genre1")).toBeTruthy();
       expect(res.body.some((x) => x.name === "genre2")).toBeTruthy();
+    });
+  });
+
+  describe("GET /:id", () => {
+    it("should return a genre if a valid id is provided", async () => {
+      const genre = new Genre({ name: "Genre1" });
+      await genre.save();
+
+      const res = await request(server).get("/api/genres/" + genre._id);
+
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty("name", genre.name);
+    });
+
+    it("should return 400 if invalid object id is provided", async () => {
+      const res = await request(server).get("/api/genres/" + 1);
+
+      expect(res.status).toBe(400);
+    });
+
+    it("should return 404 if valid id is provided but there is no data with this id", async () => {
+      const objId = mongoose.Types.ObjectId();
+      const res = await request(server).get("/api/genres/" + objId);
+
+      expect(res.status).toBe(404);
     });
   });
 });
