bc18feb058a2aa2c70359ad0ae8bdaa471965e5b
Sakibul_Munna <sakibul.munnaa@gmail.com>
2022-01-19 17:16:43 +0600

    Refactored the integration test of Get All Genres Method.

--- backend/tests/integration/genres.test.js
+++ backend/tests/integration/genres.test.js
@@ -1,17 +1,28 @@
 const request = require("supertest");
 let server;
 
+const { Genre } = require("../../models/genre");
+
 describe("/api/genres", () => {
   beforeEach(() => {
     server = require("../../index");
   });
-  afterEach(() => {
+  afterEach(async () => {
     server.close();
+    await Genre.remove();
   });
+
   describe("GET /", () => {
     it("should return all genres", async () => {
+      await Genre.collection.insertMany([
+        { name: "genre1" },
+        { name: "genre2" },
+      ]);
       const res = await request(server).get("/api/genres");
       expect(res.status).toBe(200);
+      expect(res.body.length).toBe(2);
+      expect(res.body.some((x) => x.name === "genre1")).toBeTruthy();
+      expect(res.body.some((x) => x.name === "genre2")).toBeTruthy();
     });
   });
 });
