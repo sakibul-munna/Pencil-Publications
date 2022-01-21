c659aef8246e5b8c45d539863b0ecfbff1bf9882
Sakibul_Munna <sakibul.munnaa@gmail.com>
2022-01-21 18:48:03 +0600

    Integration Testing of auth middleware.

---
+++ backend/tests/integration/auth.test.js
@@ -0,0 +1,44 @@
+const request = require("supertest");
+let server;
+
+const { Admin } = require("../../models/admin");
+const { Genre } = require("../../models/genre");
+
+describe("auth middleware", () => {
+  let token;
+
+  beforeEach(() => {
+    server = require("../../index");
+    token = new Admin().generateAuthToken();
+  });
+
+  afterEach(async () => {
+    server.close();
+    await Genre.deleteMany();
+  });
+
+  const execute = () => {
+    return request(server)
+      .post("/api/genres")
+      .set("x-auth-token", token)
+      .send({ name: "genre1" });
+  };
+
+  it("should return 401 if no token is provided", async () => {
+    token = "";
+    const res = await execute();
+    expect(res.statusCode).toBe(401);
+  });
+
+  it("should return 400 if token is invalid", async () => {
+    token = "a";
+    const res = await execute();
+    expect(res.statusCode).toBe(400);
+  });
+
+  it("should return 200 if token is valid", async () => {
+    const res = await execute();
+
+    expect(res.status).toBe(200);
+  });
+});
