5b6852236adab00239dc0d7ba8a64e57b03f777c
Sakibul_Munna <sakibul.munnaa@gmail.com>
2022-01-30 01:15:47 +0600

    Updated all the links using LinkContainer to make routing faster.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import "./Header.css";
 
 const Header = () => {
@@ -13,75 +14,98 @@ const Header = () => {
         sticky="top"
       >
         <Container>
-          <Navbar.Brand href="\">
-            <img
-              src={require("../assets/logo.png")}
-              alt="logo"
-              width="100px"
-              height="auto"
-            />
-          </Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img
+                src={require("../assets/logo.png")}
+                alt="logo"
+                width="100px"
+                height="auto"
+              />
+            </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="about" className="nav-text">
-                পেন্সিল কি
-              </Nav.Link>
-
+              <LinkContainer to="/about">
+                <Nav.Link className="nav-text">পেন্সিল কি</Nav.Link>
+              </LinkContainer>
               <NavDropdown title="আমাদের প্রকাশিত বইসমূহ" id="nav-dropdown">
-                <NavDropdown.Item clasName="text-dropdown" href="books/2019">
-                  ২০১৯
-                </NavDropdown.Item>
-                <NavDropdown.Item clasName="text-dropdown" href="books/2020">
-                  ২০২০
-                </NavDropdown.Item>
-                <NavDropdown.Item clasName="text-dropdown" href="books/2021">
-                  ২০২১
-                </NavDropdown.Item>
+                <LinkContainer to="/books/2019">
+                  <NavDropdown.Item clasName="text-dropdown">
+                    ২০১৯
+                  </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/books/2020">
+                  <NavDropdown.Item clasName="text-dropdown">
+                    ২০২০
+                  </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="/books/2021">
+                  <NavDropdown.Item clasName="text-dropdown">
+                    ২০২১
+                  </NavDropdown.Item>
+                </LinkContainer>
               </NavDropdown>
 
-              <Nav.Link href="লেখকবৃন্দ" className="nav-text">
-                লেখকবৃন্দ
-              </Nav.Link>
+              <LinkContainer to="/authors">
+                <Nav.Link className="nav-text">লেখকবৃন্দ</Nav.Link>
+              </LinkContainer>
 
               <NavDropdown title="বিষয়" id="nav-dropdown">
-                <NavDropdown.Item href="subject/উপন্যাস" className="text-dropdown" >
-                  উপন্যাস
-                </NavDropdown.Item>
-                <NavDropdown.Item href="subject/গল্প" className="text-dropdown">
-                  গল্প
-                </NavDropdown.Item>
-                <NavDropdown.Item href="subject/কবিতা" className="text-dropdown">
-                  কবিতা
-                </NavDropdown.Item>
-                <NavDropdown.Item href="subject/শিশুতোষ" className="text-dropdown">
-                  শিশুতোষ
-                </NavDropdown.Item>
-                <NavDropdown.Item href="subject/রম্য" className="text-dropdown">
-                  রম্য
-                </NavDropdown.Item>
-                <NavDropdown.Item href="subject/ভ্রমণকাহিণী" className="text-dropdown" >
-                  ভ্রমণকাহিণী
-                </NavDropdown.Item>
-                <NavDropdown.Item href="subject/প্রবন্ধ" className="text-dropdown" >
-                  প্রবন্ধ
-                </NavDropdown.Item>
-                <NavDropdown.Item href="subject/খেলাধুলা" className="text-dropdown" >
-                  খেলাধুলা
-                </NavDropdown.Item>
+                <LinkContainer to="subject/উপন্যাস">
+                  <NavDropdown.Item className="text-dropdown">
+                    উপন্যাস
+                  </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="subject/গল্প">
+                  <NavDropdown.Item className="text-dropdown">
+                    গল্প
+                  </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="subject/কবিতা">
+                  <NavDropdown.Item className="text-dropdown">
+                    কবিতা
+                  </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="subject/শিশুতোষ">
+                  <NavDropdown.Item className="text-dropdown">
+                    শিশুতোষ
+                  </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="subject/রম্য">
+                  <NavDropdown.Item className="text-dropdown">
+                    রম্য
+                  </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="subject/ভ্রমণকাহিণী">
+                  <NavDropdown.Item
+                    href="subject/ভ্রমণকাহিণী"
+                    className="text-dropdown"
+                  >
+                    ভ্রমণকাহিণী
+                  </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="subject/প্রবন্ধ">
+                  <NavDropdown.Item className="text-dropdown">
+                    প্রবন্ধ
+                  </NavDropdown.Item>
+                </LinkContainer>
+                <LinkContainer to="subject/খেলাধুলা">
+                  <NavDropdown.Item className="text-dropdown">
+                    খেলাধুলা
+                  </NavDropdown.Item>
+                </LinkContainer>
               </NavDropdown>
 
-              <Nav.Link href="ব্লগ" className="nav-text">
-                ব্লগ
-              </Nav.Link>
-              
-              <Nav.Link href="যোগাযোগ" className="nav-text">
-                যোগাযোগ
-              </Nav.Link>
+              <LinkContainer to="/contact">
+                <Nav.Link href="যোগাযোগ" className="nav-text">
+                  যোগাযোগ
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
-       
       </Navbar>
     </header>
   );
