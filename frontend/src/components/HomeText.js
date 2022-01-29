ece227fc62178fd1a6c203988f1d06fc1cc6c24a
Sakibul_Munna <sakibul.munnaa@gmail.com>
2022-01-30 01:22:10 +0600

    Refactored the the codes of HomeScreen.

---
+++ frontend/src/components/HomeText.js
@@ -0,0 +1,80 @@
+import React from "react";
+import StarIcon from "@mui/icons-material/Star";
+
+const HomeText = () => {
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <hr
+          className="my-hr"
+          style={{
+            width: "25%",
+            color: "#388761",
+            height: "2px",
+            marginRight: "20px",
+          }}
+        />
+
+        <StarIcon style={{ color: "#F0393F", fontSize: "20px" }} />
+
+        <hr
+          className="my-hr"
+          style={{
+            width: "25%",
+            color: "#388761",
+            height: "2px",
+            marginLeft: "20px",
+          }}
+        />
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h1 className="text-21">অমর একুশে গ্রন্থমেলা</h1>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <hr
+          className="my-hr"
+          style={{
+            width: "25%",
+            color: "#388761",
+            height: "2px",
+            marginRight: "20px",
+          }}
+        />
+
+        <StarIcon style={{ color: "#F0393F", fontSize: "20px" }} />
+
+        <hr
+          className="my-hr"
+          style={{
+            width: "25%",
+            color: "#388761",
+            height: "2px",
+            marginLeft: "20px",
+          }}
+        />
+      </div>
+    </>
+  );
+};
+
+export default HomeText;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,49 +4,22 @@ import books from "../books";
 import Book from "../components/Book";
 import HeaderContainer from "../components/HeaderContainer";
 import AdContainer from "../components/AdContainer";
-import StarIcon from '@mui/icons-material/Star';
+import HomeText from "../components/HomeText";
 
 const HomeScreen = () => {
   return (
     <Container fluid className="gx-0">
       <HeaderContainer />
       <AdContainer />
-      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <hr className="my-hr"
-          style={{ width: "25%", color: "#388761", height: "2px", marginRight: "20px" }}
-        />
-
-        <StarIcon style={{ color: "#F0393F", fontSize: "20px" }} />
-
-        <hr className="my-hr"
-          style={{ width: "25%", color: "#388761", height: "2px", marginLeft: "20px" }}
-        />
-      </div>
-
-      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <h1 className="text-21">অমর একুশে গ্রন্থমেলা</h1>
-      </div>
-
-      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <hr className="my-hr"
-          style={{ width: "25%", color: "#388761", height: "2px", marginRight: "20px" }}
-        />
-
-        <StarIcon style={{ color: "#F0393F", fontSize: "20px" }} />
-
-        <hr className="my-hr"
-          style={{ width: "25%", color: "#388761", height: "2px", marginLeft: "20px" }}
-        />
-      </div>
-
-      <Container>
-      <Row>
-        {books.map((book) => (
-          <Col sm={12} md={6} lg={4} xl={3}>
-            <Book book={book} />
-          </Col>
-        ))}
-      </Row>
+      <HomeText />
+      <Container className="py-3">
+        <Row>
+          {books.map((book) => (
+            <Col sm={12} md={6} lg={4} xl={3}>
+              <Book book={book} />
+            </Col>
+          ))}
+        </Row>
       </Container>
     </Container>
   );
