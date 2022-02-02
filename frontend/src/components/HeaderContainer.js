5267c50f5ced04f4a92a0add88f40af6892912e4
Sakibul_Munna <sakibul.munnaa@gmail.com>
2022-02-02 12:19:46 +0600

    Updated HeaderContainer with font-awesome icons.

--- frontend/src/components/HeaderContainer.js
+++ frontend/src/components/HeaderContainer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Button } from "react-bootstrap";
 import "./HeaderContainer.css";
+import { MDBIcon } from "mdb-react-ui-kit";
 
 const HeaderContainer = () => {
   return (
@@ -23,7 +24,7 @@ const HeaderContainer = () => {
           <div className="headerContent-button">
             <a href="https://www.facebook.com/groups/pencilglobal">
               <Button variant="outline-danger">
-                <FacebookIcon style={{ color: "#3b5998" }} />
+                <MDBIcon icon="facebook-f" className="mx-3" />
                 পেন্সিল ফেসবুক গ্রুপে জয়েন করার জন্য এইখানে ক্লিক করুন।
               </Button>{" "}
             </a>
