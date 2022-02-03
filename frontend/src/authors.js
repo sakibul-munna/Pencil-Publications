5ebfe69419919ff05b9dd2c133a4cd307a58b140
Sakibul_Munna <sakibul.munnaa@gmail.com>
2022-02-04 00:42:26 +0600

    Added some authors data.

---
+++ frontend/src/authors.js
@@ -0,0 +1,59 @@
+const author = [
+  {
+    _id: 1,
+    name: "অল্পনা তালুকদার",
+    image: "/images/authors/Alpona_Talukder.jpg",
+  },
+  {
+    _id: 2,
+    name: "আনোয়ার এম হোসাইন",
+    image: "/images/authors/Anwar m Hossain.jpg",
+  },
+  {
+    _id: 3,
+    name: "অরণ্য আহমেদ",
+    image: "/images/authors/Aronno Ahmed.jpg",
+  },
+  {
+    _id: 4,
+    name: "আসাদুল লতিফ",
+    image: "/images/authors/Ashadul Latif.jpg",
+  },
+  {
+    _id: 5,
+    name: "আসাদুজ্জামান লতিফ",
+    image: "/images/authors/Ashaduzzaman Jwuel.jpg",
+  },
+  {
+    _id: 6,
+    name: "বিপাশা বাশার",
+    image: "/images/authors/Bipasha_Bashar.jpg",
+  },
+  {
+    _id: 7,
+    name: "দেবব্রত পাল",
+    image: "/images/authors/Debobroto Pual.jpg",
+  },
+  {
+    _id: 8,
+    name: "ডঃ কালাম মীর",
+    image: "/images/authors/Dr. Kalam Mir.JPG",
+  },
+  {
+    _id: 9,
+    name: "এমিলা নূর",
+    image: "/images/authors/Emila Nur.jpeg",
+  },
+  {
+    _id: 10,
+    name: "এশরার লতিফ",
+    image: "/images/authors/Eshrar Latif.jpg",
+  },
+  {
+    _id: 11,
+    name: "তৌহিদ হাসান",
+    image: "/images/authors/Touhin_Hassan.jpg",
+  },
+];
+
+export default author;
