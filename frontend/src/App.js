48cbf427d73499a2332722c814c3d4067f80f99b
Sakibul_Munna <sakibul.munnaa@gmail.com>
2022-02-04 23:59:53 +0600

    Connected AdminLogin Screen into the main app using some conditional rendering.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import { Container } from "react-bootstrap";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
 import AboutScreen from "./screens/AboutScreen";
 import ContactScreen from "./screens/ContactScreen";
 import AuthorScreen from "./screens/AuthorScreen";
+import AdminLoginScreen from "./screens/AdminLoginScreen";
 import Footer from "./components/Footer";
 
 const App = () => {
+  let location = useLocation();
   return (
     <Container fluid className="gx-0">
       <Header />
@@ -16,8 +18,9 @@ const App = () => {
         <Route exact path="/about" element={<AboutScreen />} />
         <Route exact path="/contact" element={<ContactScreen />} />
         <Route exact path="/authors" element={<AuthorScreen />} />
+        <Route exact path="/admin/login" element={<AdminLoginScreen />} />
       </Routes>
-      <Footer />
+      {location.pathname !== "/admin/login" && <Footer />}
     </Container>
   );
 };
