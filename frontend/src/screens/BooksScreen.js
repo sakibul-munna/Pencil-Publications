e345f06560d5a0511221561fa5c5eefd96be1c6e
Sakibul_Munna <sakibul.munnaa@gmail.com>
2022-03-06 17:48:10 +0600

    No Books Available Section Added to BooksScreen.

--- frontend/src/screens/BooksScreen.js
+++ frontend/src/screens/BooksScreen.js
@@ -20,15 +20,28 @@ const BooksScreen = ({ headerText, selectedYear }) => {
   return (
     <Container fluid style={{ minHeight: "600px" }} className="gx-0 my-4">
       <HomeText headerText={headerText} />
-      <Container className="py-3">
-        <Row>
-          {selectedBooks.map((book) => (
-            <Col sm={12} md={6} lg={4} xl={3}>
-              <BookCard book={book} />
-            </Col>
-          ))}
-        </Row>
-      </Container>
+      {selectedBooks.length > 0 ? (
+        <Container className="py-3">
+          <Row>
+            {selectedBooks.map((book) => (
+              <Col sm={12} md={6} lg={4} xl={3}>
+                <BookCard book={book} />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      ) : (
+        <Container
+          className="my-4"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h1>No Books Available</h1>
+        </Container>
+      )}
     </Container>
   );
 };
